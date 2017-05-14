3751ff56bed2032c62d2a4c29800c00ef29de9df
Raiden Dilan <raiden18@me.com>
2017-05-14 20:58:37 +0100

    complete website css redesign

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,11 @@
 angular
-  .module('pncApp', ['ui.router', 'ngResource', 'satellizer', 'ui.bootstrap', 'ngAnimate', 'ngMessages', 'ngMap', 'chart.js']);
+  .module('pncApp', [
+    'ui.router',
+    'ngResource',
+    'satellizer',
+    'ui.bootstrap',
+    'ngAnimate',
+    'ngMessages',
+    'ngMap',
+    'chart.js'
+  ]);
